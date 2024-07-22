f923b895e60d7c6e632582158d865403fdb107ed
Emiliano Davila <emilianommdavila@gmail.com>
2024-07-21 22:33:13 -0300

    primer commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./header.css"
+import LOGO from "../../assets/icons/iconoHome.png"
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <a href="#home" className='nav_logo'>
+        <img src={LOGO} className="nav_logo_image" alt="" />
+        <div className='nav_name'>
+          <h1 className='nav_name_name'>Luciana Mateo</h1>
+          <h2 className='nav_name_title'>Animadora Digital</h2>
+        </div>
+      </a>
+
+      <nav className='nav'>
+        <div className='nav_menu'>
+          <ul className='nav_item'>
+            <a href="">Sobre mi</a>
+          </ul>
+          <ul className='nav_item'>
+            <a href="">Habilidades</a>
+          </ul>
+          <ul className='nav_item'>
+            <a href="">Proyectos</a>
+          </ul>
+          <ul className='nav_item'>
+            <a href="">Contacto</a>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
