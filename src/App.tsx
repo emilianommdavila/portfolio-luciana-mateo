9c5f291283c5a1c46d5656b20a047e0c9b7bf9db
Emiliano Davila <emilianommdavila@gmail.com>
2024-07-21 22:38:06 -0300

    primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import "./header.css"
 import LOGO from "../../assets/icons/iconoHome.png"
 
