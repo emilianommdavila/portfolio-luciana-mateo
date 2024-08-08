80e4906d44003dae9e445f12113aa287cdda208f
Emiliano Davila <emilianommdavila@gmail.com>
2024-08-07 22:57:51 -0300

    botones

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,8 +1,10 @@
 //import React from 'react'
-import { useRef,useState } from "react";
+import { useRef, useState } from "react";
 import "./projects.css";
 import LEFTARROW from "../../assets/icons/leftArrow.png";
 import RIGHTARROW from "../../assets/icons/rigthArrow.png";
+import GREYDOT from "../../assets/icons/dotGrey.png";
+import LIGTHDOT from "../../assets/icons/dotGreyLigth.png";
 
 
 
@@ -35,6 +37,17 @@ const Projects = (props: Section) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const [currentIndexIntern, setCurrentIndexIntern] = useState(0);
+
+  const buttonStyle = selectedProject.content.length === 1 ? { display: 'none' } : { display: 'relative' };
+
+  const centerStyle = selectedProject.content.length === 1 ? {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%'
+  } : {};
+
+
 
   const alterLanding = (project: Project) => {
     setSelectedProject(project);
@@ -114,77 +127,87 @@ const Projects = (props: Section) => {
       console.log("cambie" + item.url)
       return (
         <video key={item.url} className="carrouselVideo" controls width="360" height="240">
-        <source src={item.url} type='video/mp4' />
-      </video>
+          <source src={item.url} type='video/mp4' />
+        </video>
       );
     }
     return null;
   };
 
 
-  const returnSection = ()=>{
+  const returnSection = () => {
 
     return (
       <div className="projects">
-      <div className="title">{name}</div>
-      <div className="GridContainer">
-        <button className="navButton left" onClick={handlePrev}>
-          <img className="navArrow" src={LEFTARROW} alt="" />
-        </button>
-        <div
-          className="Grid"
-          ref={gridRef}
-          style={{width: `100%`/*aca tengo que cambiar el desplazamiento de la balla*/ }}
-        >
-          {texts.projects.map((project, index) => (
-            <div key={index}>{obtainProjects(project)}</div>
-          ))}
-        </div>
-
-        <button className="navButton right" onClick={handleNext}>
-          <img className="navArrow" src={RIGHTARROW} alt="" />
-        </button>
-      </div>
-      <div className="landing">
-        <div className="carrouselComplete">
-          <div className="carrousel">
-            <div className="carrousel-background"
-              style={{
-                backgroundImage: 'url('+selectedProject.frontImage+')',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                filter: 'blur(5px)'
-              }}></div>
-            <div className="carrousel-content">
-              <button
-                className="navButton small left"
-                onClick={handleCarouselPrev}
-              >
-                <img className="navArrow" src={LEFTARROW} alt="" />
-              </button>
-              {renderCarouselItem(selectedProject.content[currentIndexIntern])}
-
-              <button
-                className="navButton small right"
-                onClick={handleCarouselNext}
-              >
-                <img className="navArrow" src={RIGHTARROW} alt="" />
-              </button>
-            </div>
-          </div>
-          <div className="carouselIndicators">
-
-            {selectedProject.content.map((_, idx) => (
-              <span key={idx} onClick={() => setCurrentIndexIntern(idx)}>{`${idx === currentIndexIntern ? '🔴' : '⚪'}`}</span>
+        <div className="title">{name}</div>
+        <div className="GridContainer">
+          <button className="navButton left" onClick={handlePrev}>
+            <img className="navArrow" src={LEFTARROW} alt="" />
+          </button>
+          <div
+            className="Grid"
+            ref={gridRef}
+            style={{ width: `100%`/*aca tengo que cambiar el desplazamiento de la balla*/ }}
+          >
+            {texts.projects.map((project, index) => (
+              <div key={index}>{obtainProjects(project)}</div>
             ))}
           </div>
+
+          <button className="navButton right" onClick={handleNext}>
+            <img className="navArrow" src={RIGHTARROW} alt="" />
+          </button>
         </div>
-        <div className="Description">
-          <h2 className="DescriptionTitle">{selectedProject.name}</h2>
-          <p className="DescrioptionDescription">{selectedProject.description}</p>
+        <div className="landing">
+          <div className="carrouselComplete">
+            <div className="carrousel">
+              <div className="carrousel-background"
+                style={{
+                  backgroundImage: 'url(' + selectedProject.frontImage + ')',
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover',
+                  filter: 'blur(5px)'
+                }}></div>
+              <div className="carrousel-content" style={centerStyle}>
+                <button
+                  className="navButton small left"
+                  onClick={handleCarouselPrev}
+                  style={buttonStyle}
+                >
+                  <img className="navArrow" src={LEFTARROW} alt="" />
+                </button>
+                {renderCarouselItem(selectedProject.content[currentIndexIntern])}
+
+                <button
+                  className="navButton small right"
+                  onClick={handleCarouselNext}
+                  style={buttonStyle}
+                >
+                  <img className="navArrow" src={RIGHTARROW} alt="" />
+                </button>
+              </div>
+            </div>
+            <div className="carouselIndicators">
+
+            {selectedProject.content.map((_, idx) => (
+  <span style={buttonStyle} key={idx} onClick={() => setCurrentIndexIntern(idx)}>
+    {idx === currentIndexIntern ? (
+      <img className="dotImage" src={GREYDOT} alt="" />
+    ) : (
+      <img className="dotImage" src={LIGTHDOT} alt="" />
+    )}
+  </span>
+))}
+
+              
+            </div>
+          </div>
+          <div className="Description">
+            <h2 className="DescriptionTitle">{selectedProject.name}</h2>
+            <p className="DescrioptionDescription">{selectedProject.description}</p>
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 
@@ -193,8 +216,8 @@ const Projects = (props: Section) => {
 
 
   return (
-    <div className="projectsContainer">      
-     { returnSection()}
+    <div className="projectsContainer">
+      {returnSection()}
     </div>
   );
 };
